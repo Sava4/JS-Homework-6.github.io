fafd158d233c46bbe5e54f248a35310701f6f09b
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-11 12:48:04 +0300

    extract checkType to function, check object vs null

--- script.js
+++ script.js
@@ -2,12 +2,54 @@
 
 let array = ['1', 2, '3', null, 20, undefined, NaN, 9, null, 42, 'string', null, '4', 5];
 
+// const filterBy = (arr, dType) => {
+//     return (arr.filter(el => typeof el !== dType))
+// }
+
+const checkType = (arg) => {
+    switch (typeof (arg)) {
+        case 'number':
+            return 'number';
+            break;
+        case 'boolean':
+            return 'boolean';
+            break;
+        case 'undefined':
+            return 'undefined';
+            break;
+        case 'bigint':
+            return 'bigint';
+            break;
+        case 'string':
+            return 'string';
+            break;
+        case 'symbol':
+            return 'symbol';
+            break
+        case 'function':
+            return 'function';
+            break;
+            //Object vs Null check
+        case 'object':
+            if (arg === null) {
+                return 'null';
+            } else return 'object';
+    }
+} 
+
 const filterBy = (arr, dType) => {
-    return (arr.filter(el => typeof el !== dType))
+    let resultarr = []
+    arr.forEach(el => { 
+        if (checkType(el) !== dType) {
+            resultarr.push(el);         
+            }
+        })
+    return resultarr
 }
 
 console.log('This is a Boolean filter: ', filterBy(array, "boolean"));
-console.log('This is a Null filter: ', filterBy(array, "object"));
+console.log('This is a Null filter: ', filterBy(array, "null"));
+console.log('This is an Object filter:', filterBy(array, "object"));
 console.log('This is an Undefined filter: ', filterBy(array, "undefined"));
 console.log('This is a Number filter: ', filterBy(array, "number"));
 console.log('This is a BigInt filter: ', filterBy(array, "bigint"));
